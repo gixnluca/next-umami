4296c18aa269550bea5bc676baac3eee178f5cd4
Gianluca <gianlucademarchi9@gmail.com>
2024-08-04 13:12:06 +0400

    chore: demo updates

--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -10,11 +10,6 @@ module.exports = {
       colors: {
         brand: '#147AF3',
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
